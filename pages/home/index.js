09ff3beca10ed6811f74d63fd5d666eafeec73b3
Técnico Informática <56121742019.1@senacrjedu.local>
2021-11-24 19:24:24 -0300

    View concluidos

--- pages/home/index.js
+++ pages/home/index.js
@@ -7,71 +7,61 @@
  * Controller da página 'home'.
  */
 
- $(document).ready(runHome);
+$(document).ready(runHome);
 
- function runHome() {
- 
-     // Título da página
-     setTitle();
- 
-     // Monitora eventos de clique nos artigos
-     $(document).on('click', '.art-block', viewArticle);
- 
-     // Lê todos os artigos do banco de dados e armazena na variável 'querySnapshot'
-     db.collection('articles')
-         .where('status', '==', 'ativo')
-         .orderBy('date', 'desc')
-         .onSnapshot((querySnapshot) => {
- 
-             // Variável com view dos artigos
-             var articles = '';
- 
-             // Obtém cada artigo contido em 'querySnapshot' e armazena na variável 'doc'
-             querySnapshot.forEach((doc) => {
- 
-                 // Obtém os campos e valores do documento e armazena na variável 'article'
-                 var article = doc.data();
- 
-                 // Obtém o id documento e armazena em 'article.id'
-                 article.id = doc.id;
- 
-                 // Monta a view da página
-                 articles += `
- <div class="art-block" title="${article.title}" data-id="${article.id}">
-     <div class="art-block-image" style="background-image: url('${article.image}')"></div>
-     <div class="art-block-content">
-         <h3>${article.title}</h3>
-         <small>${article.resume}</small>
-         ${article.date}
-     </div>
- </div>
- `;
- 
-             });
- 
-             $('#viewArticles').html(articles);
- 
-         });
- 
- }
- 
- // Processa cliques nos artigos
- function viewArticle() {
- 
-     // Obtém o atributo 'data-id' do elemento clicado
-     articleId = sanitizeString($(this).attr('data-id'));
- 
-     // Exibe o artigo completo
-     loadPage(`view?${articleId}`);
- 
- }
- 
- // Limpa caracteres perigosos das strings
- function sanitizeString(badString, stripHTML = true) {
- 
-     // Remove espaços em excesso da string
-     goodString = badString.trim();
- 
-     // Retorna a string sanitizada
-     return goodString;
- }
+function runHome() {
+
+    // Título da página
+    setTitle();
+
+    // Monitora eventos de clique nos artigos
+    $(document).on('click', '.art-block', viewArticle);
+
+    // Lê todos os artigos do banco de dados e armazena na variável 'querySnapshot'
+    db.collection('articles') // Nome da coleção
+        .where('status', '==', 'ativo') // Somente com status ativo
+        .where('date', '<=', getSystemDate()) // Somente data atual e no passado (agendamento)
+        .orderBy('date', 'desc') // Ordenado pela data mais recente
+        .onSnapshot((querySnapshot) => {
+
+            // Variável com view dos artigos
+            var articles = '';
+
+            // Obtém cada artigo contido em 'querySnapshot' e armazena na variável 'doc'
+            querySnapshot.forEach((doc) => {
+
+                // Obtém os campos e valores do documento e armazena na variável 'article'
+                var article = doc.data();
+
+                // Obtém o id documento e armazena em 'article.id'
+                article.id = doc.id;
+
+                // Monta a view da página
+                articles += `
+<div class="art-block" title="${article.title}" data-id="${article.id}">
+    <div class="art-block-image" style="background-image: url('${article.image}')"></div>
+    <div class="art-block-content">
+        <h3>${article.title}</h3>
+        <small>${article.resume}</small>
+    </div>
+</div>
+`;
+
+            });
+
+            $('#viewArticles').html(articles);
+
+        });
+
+}
+
+// Processa cliques nos artigos
+function viewArticle() {
+
+    // Obtém o atributo 'data-id' do elemento clicado
+    articleId = sanitizeString($(this).attr('data-id'));
+
+    // Exibe o artigo completo
+    loadPage(`view?${articleId}`);
+
+}
--- pages/view/index.js
+++ pages/view/index.js
@@ -1,17 +1,74 @@
 /**
- * index.js
+ * view/index.js
  * 
  * Created by André Luferat → http://www.luferat.net/
  * The MIT License (http://www.opensource.org/licenses/mit-license.php)
  * 
- * Controller da página 'about'.
+ * Controller da página 'view'.
  */
 
-$(document).ready(runAbout);
+$(document).ready(runView);
 
-function runAbout() {
+// Armazena a view do artigo
+var artView = '';
+
+function runView() {
 
     // Título da página
-    setTitle('Sobre...');
+    setTitle('Artigo completo');
+
+    // Obtém o Id do artigo
+    var articleId = sanitizeString(location.search.replace('?', ''));
+
+    // Obtém o artigo completo, conforme o Id
+    getArticle(articleId);
+
+}
+
+// Lê artigo completo do Firestore
+function getArticle(articleId) {
+
+    // Obtém o documento do artigo
+    db.collection('articles').doc(articleId).get()
+
+        // Se deu certo...       
+        .then((doc) => {
+
+            // Se o artigo existe
+            if (doc.exists) {
+
+                // Armazena documento em 'art'
+                var art = doc.data();
+
+                console.log("--");
+
+                // Título da página
+                setTitle(art.title);
+
+                // Converte data para Br
+                var brDate = getBrDate(art.date);
+
+                artView += `
+<h2>${art.title}</h2>
+<small class="author-date">Por ${art.author} em ${brDate}.</small>
+<div class="content">${art.text}</div>
+            `;
+
+                // Se o artigo não existe
+            } else {
+                artView += `
+<h2>Ooops!</h2>
+<p>Artigo não encontrado.</p>
+`;
+            }
+
+            // Exibe na view
+            $('#artView').html(artView);
+        })
+
+        // Se deu errado...
+        .catch((error) => {
+            console.erro("Ooops! Algo deu errado:", error);
+        });
 
 }
